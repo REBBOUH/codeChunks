63919378290c93c8dea5b96ea44083fe4502bb53
Belkacem REBBOUH <belkacem@rebbouh.com>
2014-03-05 17:22:19 +0100

    Create regasm.cmd

---
+++ Csharp/regasm.cmd
@@ -0,0 +1,8 @@
+create assembly regasm 
+with output type console application
+Create an internal sealed class that implements ITypeLibExporterNotifySink
+
+from the dll name 
+use  RegistrationServices
+get the registrable types GetRegistrableTypesInAssembly(assembly)
+
