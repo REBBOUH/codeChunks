2f6f6278cd19f7fe147ec0dc9936b8ef3efb9c4c
Belkacem REBBOUH <Belkacem@users.noreply.github.com>
2015-09-23 18:41:14 +0200

    Create HttpClientStreams.java

---
+++ mongodb/HttpClientStreams.java
@@ -0,0 +1,13 @@
+    @Nullable
+    private static InputStream executeRequest(WebTarget target, Entity entity, boolean acceptGzip) {
+        InputStream stream = null;
+        if (entity != null) {
+            long start = currentTimeMillis();
+            stream = getRequest(target, acceptGzip)
+                    .accept(APPLICATION_OCTET_STREAM_TYPE)
+                    .post(entity, InputStream.class);
+            logger.info("Request took {} s", MILLISECONDS.toSeconds(currentTimeMillis() - start));
+        }
+        return stream;
+    }
+    
