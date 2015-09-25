9272e9c3b29a53f998f73de6050f9dbdda0fa807
Belkacem REBBOUH <Belkacem@users.noreply.github.com>
2015-09-25 17:44:13 +0200

    Update 3-HttpClientStreams.java

--- mongodb/3-HttpClientStreams.java
+++ mongodb/3-HttpClientStreams.java
@@ -12,4 +12,27 @@
     }
     
     
-    
+    @GET
+    @Path("/{date}")
+    @Produces(MediaType.TEXT_PLAIN)
+    public Response sendStore(
+            @PathParam("date") final String date,
+            @QueryParam("name") final List<String> names
+    ) {
+        final LocalDate storeDate;
+        try {
+            storeDate = parse(date);
+        } catch (IllegalArgumentException e) {
+            return ServerApiUtils.buildErrorResponse("Invalid date " + date + " please use this format yyyy-MM-dd Ex : 2015-01-22.");
+        }
+        if (names == null || names.isEmpty()) {
+            return noContent().entity("No elements found for this store " + date).build();
+        }
+        return ok(new StreamingOutput() {
+            @Override
+            public void write(OutputStream output) throws IOException, WebApplicationException {
+                // Send request to database and stream the cursor results or use reactive-streams for ex.
+                output.close();
+            }
+        }).build();
+    }
