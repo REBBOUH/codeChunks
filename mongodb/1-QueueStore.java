f970a25e685f124ff08c9310a02874b405e0c3ca
Belkacem REBBOUH <Belkacem@users.noreply.github.com>
2015-10-05 22:24:38 +0200

    Update 1-QueueStore.java

--- mongodb/1-QueueStore.java
+++ mongodb/1-QueueStore.java
@@ -1,7 +1,7 @@
 
 public class JobQueueStore implements QueueStore<Job> {
     @Override public void store(Long id, Job value) {
-        dbCollection.update(getCriteria(id), getBasicDBObject(key, value), true, false);
+        dbCollection.insert(getBasicDBObject(key, value));
     }
     @Override public void delete(Long key) {
         // delete the job from the collection using it's id.
